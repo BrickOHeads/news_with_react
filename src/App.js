c6e8a1b479631cd09832668ba787872c376931c4
Brittany  Arsi <brittmacarsi@icloud.com>
2017-08-13 09:15:23 -0400

    fixed some issues and increased the amount of articles returned

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React, {Component} from 'react';
 import './App.css';
 
 class Feed extends Component {
-
   render() {
     // ********Receive state from parent component via props.
     let feed = this.props.feed
     let renderFeed = feed.map((article) => {
       // ********The API returns a blank img placeholder if there is no image.
-      // ******We will change that to an actual image for a more polished finish.
       let imgUrl;
       if (article.urlToImage === 'http://assets1.ignimgs.com/2015/05/27/contentplaceholderpng-967b4c.png') {
         imgUrl = '../images/IGN_Entertainment_Logo.svg.png';
@@ -18,21 +16,21 @@ class Feed extends Component {
       }
       // *******Set variables for each article property.
       let author = article.author,
-      description = article.description,
-       title = article.title,
-       url = article.url,
-        // *******Since no property from the article object is suitable as a string, we create a //********random string for each.
-        key = Math.random().toString(36).substring(7);
+          description = article.description,
+          title = article.title,
+          url = article.url,
+        // *******create a random string//********
+          key = Math.random().toString(36).substring(7);
       return (
         <div key={key} className="col-sm-6 col-md-4">
           <div className="thumbnail">
             <img className="" src={imgUrl} alt={description}/>
             <div className="caption">
-              <p className="lead truncate">{title}</p>
-              <hr className="divider"/>
-              <p className="text-center truncate">{description}</p>
-              <span className="text-center pull-left">{author || 'IGN'}</span>
-              <a href={url} className="btn btn-danger" target="_blank">Read more!</a>
+                  <p className="lead truncate">{title}</p>
+                  <hr className="divider"/>
+                  <p className="text-center truncate">{description}</p>
+                  <span className="text-center pull-left">{author || 'IGN'}</span>
+                  <a href={url} className="btn btn-danger" target="_blank">Read more!</a>
             </div>
           </div>
         </div>
@@ -44,7 +42,6 @@ class Feed extends Component {
         <hr className="divider"/>
         <div className="col-md-12">
           <div className="row">
-
           {this.props.children}
           {renderFeed}
           </div>
@@ -86,7 +83,7 @@ class App extends Component {
     let randomArray = [];
     //*********** We use a for loop to iterate over the length of the array six time.
     // ********Then we grab a random news headline in each iteration, removing duplicates.
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 9; i++) {
       let index = Math.floor(Math.random() * tmp.length);
       let removed = tmp.splice(index, 1);
       // ********Since we are only removing one element
@@ -106,7 +103,7 @@ class App extends Component {
   componentWillMount() {
   // *******Set your API URL with the API key.
   let url = "https://newsapi.org/v1/articles?source=new-scientist&sortBy=top&apiKey=9f23ca15a0b648f9a5dd1b6fa3cfce4b"
-  // ********We use regex to extra website name.
+  // ********We use regex to extra website name. pop returns the last element of array
   let extract = url.match(/source=\=*(.*?)\s*&s/).pop();
   // *******We set site name to state.
   this.setState({site: extract})
